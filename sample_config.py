d1e62941a626a251102c7866554857238d9b83a0
D S I GUNASEKARA <148281527+shamitum@users.noreply.github.com>
2024-01-14 14:12:20 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,19 +2,19 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6950371339:AAE19lbdprs_jSWhwDrhT6gOoKBc9BCsGvc")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", 1415735))
+    API_HASH = os.environ.get("API_HASH", "58af626343da6313c01ba24489d3d550")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6673162846").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
-    MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 2097152000
-    FREE_USER_MAX_FILE_SIZE = 50000000
+    MAX_FILE_SIZE = 4194304000
+    TG_MAX_FILE_SIZE = 4194304000
+    FREE_USER_MAX_FILE_SIZE = 4194304000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
